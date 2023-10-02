ed426d4aa3a7e9624488d56036120284282012c3
rajchaudhry61 <145329238+rajchaudhry61@users.noreply.github.com>
2023-10-02 05:52:10 +0000

    cons.js

--- 01_basic/03cons.js
+++ 01_basic/03cons.js
@@ -1,14 +1,21 @@
-let assume = 22a
- console.log(typeof assume);
+let score = 22
+ //console.log(typeof assume);
 
- let valueInNumber = Number(num);
+ let valueInNumber = Number(score);
 
- console.log(typeof assume);
- console.log((typeof assume));
+ //console.log(typeof assume);
+ //console.log((typeof assume));
 
 
  // 22=> number
  // "22"=> string
+ // 22abc=> NaN
+ // true => 1; false=>0
+
+ let isLoggedIn=1
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
 
  let anyNumber = 52
  let stringNumber = String(anyNumber)
