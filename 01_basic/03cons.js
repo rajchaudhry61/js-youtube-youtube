ad7bece3a5ed986ca475e6b57f0742fe28b294c9
rajchaudhry61 <145329238+rajchaudhry61@users.noreply.github.com>
2023-10-02 05:06:56 +0000

    03cons.js

---
+++ 01_basic/03cons.js
@@ -0,0 +1,16 @@
+let assume = 22a
+ console.log(typeof assume);
+
+ let valueInNumber = Number(num);
+
+ console.log(typeof assume);
+ console.log((typeof assume));
+
+
+ // 22=> number
+ // "22"=> string
+
+ let anyNumber = 52
+ let stringNumber = String(anyNumber)
+ console.log(anyNumber);
+ console.log(typeof anyNumberr);
